0332511fac1f821835380930cef1f8c2b194641d
brenda70904 <brendaj0214@gmail.com>
2022-10-04 18:42:39 -0700

    fix spelling and adding bio

--- js/app.js
+++ js/app.js
@@ -4,19 +4,18 @@ let username = prompt('What\'s your name?');
 console.log(username);
 alert(`Welcome ${username} !`);
 //instruction
-alert('Let\'s play a guessing game! there will be 5 questions, let\'s see how many can you get it right! Please answer the following question with true or false.');
+alert('Let\'s play a guessing game! there will be 5 questions, let\'s see how many you can get right! Please answer the following question with true or false.');
 alert(`Are you ready? ${username}`);
 
 // question 1
 let nameQuestion = prompt('My name starts with J').toLowerCase();
-console.log(nameQuestion);
 
 if (nameQuestion === 'false' || nameQuestion === 'f' || nameQuestion === 'no' || nameQuestion === 'n') {
   console.log('user enter correct answer');
-  alert(`you are CORRECT ${username} !`);
+  alert(`you are CORRECT ${username} ! My name strts with B.`);
 } else if (nameQuestion === 'true' || nameQuestion === 't' || nameQuestion === 'yes' || nameQuestion === 'y') {
   console.log('user enter wrong answer');
-  alert(`Bzzzz! sorry,wrong answer ${username}, my name starts with B`);
+  alert(`Bzzzz! sorry,wrong answer ${username}, my name starts with B.`);
 } else {
   console.log('user is not following the direction');
   alert('you need to answer with true or false');
@@ -25,10 +24,10 @@ if (nameQuestion === 'false' || nameQuestion === 'f' || nameQuestion === 'no' ||
 let foodQuestion = prompt('I don\'tlike watermelon.').toLowerCase();
 if (foodQuestion === 'true' || foodQuestion === 't' || foodQuestion === 'yes' || foodQuestion === 'y') {
   console.log('user enter correct answer');
-  alert(`you are CORRECT ${username} ! I don't like watermelon! Yah I know I'm werid lol`);
+  alert(`you are CORRECT ${username} ! I don't like watermelon! Yeah I know I'm weird  lol`);
 } else if (foodQuestion === 'false' || foodQuestion === 'f' || foodQuestion === 'no' || foodQuestion === 'n') {
   console.log('user enter wrong answer');
-  alert(`Bzzzz! sorry, wrong answer ${username}, I don't like watermelon. Yahh I know I'm werid lol `);
+  alert(`Bzzzz! sorry, wrong answer ${username}, I don't like watermelon! Yeah I know I'm weird`);
 } else {
   console.log('user is not following the direction');
   alert('you need to answer with true or false');
@@ -52,7 +51,7 @@ if (sweetQeustion === 'true' || sweetQeustion === 't' || sweetQeustion === 'yes'
   alert(`Bzzzz! sorry, wrong answer ${username}, I love milk tea! my favorite milk tea place is Happy Lemon and Young tea in Bellevue.`);
 } else if (sweetQeustion === 'false ' || sweetQeustion === 'f' || sweetQeustion === 'no' || sweetQeustion === 'n') {
   console.log('user enter correct answer');
-  alert(`you are CORRECT ${username} ! I love milk tea! 30% sugar level with less ice. yumm! By the way, my favorite milk tea place is Happy Lemon and Young tea in Bellevue..`);
+  alert(`you are CORRECT ${username} ! I love milk tea! 30% sugar level with less ice. Yum! By the way, my favorite milk tea place is Happy Lemon and Young tea in Bellevue.`);
 } else {
   console.log('user is not following the direction');
   alert('you need to answer with true or false');
@@ -62,7 +61,7 @@ if (sweetQeustion === 'true' || sweetQeustion === 't' || sweetQeustion === 'yes'
 let bugQuestion = prompt('I love bugs').toLowerCase();
 if (bugQuestion === 'true' || bugQuestion === 't' || bugQuestion === 'yes' || bugQuestion === 'y') {
   console.log('user enter wrong answer');
-  alert(`NO! NO! NO sorry, wrong answer ${username}! I hate bugs. I scream everytime when I see bugs. but, as a Software developer, who like bugs? `);
+  alert(`NO! NO! NO sorry, wrong answer ${username}! I hate bugs. I scream every time I see bugs. But, as a Software developer, who likes bugs?`);
 } else if (bugQuestion === 'false' || bugQuestion === 'f' || bugQuestion === 'no' || bugQuestion === 'n') {
   console.log('user enter correct answer');
   alert(`you are CORRECT ${username} ! oh well, as a software engineer, who likes bugs anyway?`);
